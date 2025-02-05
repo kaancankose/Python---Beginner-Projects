adccae998248a7dc18ff775b55a72024690ed9a7
Kaan Can Köse <kaancankose1923@gmail.com>
2025-02-05 15:05:56 +0300

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+print("Merhaba, Dünya!")
+
+isim = "Ali"
+print("Merhaba,", isim)
+
+yas = 25
+print(f"Ben {yas} yaşındayım.")
+
+print("Python", "öğrenmek", "çok", "eğlenceli!", sep="-")
+
+print("Bu birinci satır.", end=" ")
+print("Bu ikinci satır.")
+
+print("Satır atlamak için \\n kullanılır.\nYeni satırdayız!")
+
+liste = [1, 2, 3, 4]
+sozluk = {"ad": "Mehmet", "yas": 30}
+print("Liste:", liste)
+print("Sözlük:", sozluk)
+
+print("5 + 3 =", 5 + 3)
+
